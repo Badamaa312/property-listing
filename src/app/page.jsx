a409c9672d67b2d7ba7728ca82f06a3f66a039fc
Бадамгарав Орги <24LP1595@administrators-iMac-2.local>
2024-09-18 12:16:13 +0800

    Mock data oruulj filtered

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,42 @@
+"use client";
+
 import { LogoIcon } from "@/components/svg/LogoIcon";
 import { SearchIcon } from "@/components/svg/SearchIcon";
 import Image from "next/image";
 import Link from "next/link";
 import { Map } from "@/components/map/GoogleMap";
+import data from "../mock/data.json";
+import { useState, useEffect } from "react";
+import { SearchDropDown } from "@/components/search/SearchDropDown";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  if (typeof window !== "undefined") {
+    document.addEventListener("mouseup", () => {
+      handleCloseDropDown();
+    });
+  }
+  const filteredProperty = data.properties.filter((property) =>
+    property?.title?.toLowerCase().includes(searchValue)
+  );
+
+  console.log(filteredProperty);
+
+  const handleCloseDropDown = () => {
+    setIsOpen(false);
+  };
+
+  const handleInputChange = (event) => {
+    setIsOpen(true);
+    setSearchValue(event.target.value);
+  };
+
+  // useEffect(() => {
+  //   fetchSearchData();
+  // }, []);
+
   return (
     <main className="pb-8 pt-8">
       <div className="w-full flex flex-col gap-10 justify-center items-center">
@@ -17,23 +49,24 @@ export default function Home() {
             <div className="btn btn-outline btn-accent dropdown-left">Rent</div>
             <input
               placeholder="Search"
-              //   value={searchValue}
+              value={searchValue}
               type="text"
               className="bg-[#E8E8EA] outline-none rounded-md"
-              //   onChange={handleInputChange}
+              onChange={handleInputChange}
             />
             <div className="btn btn-outline btn-accent ">
               <SearchIcon />
             </div>
+            {/* <p>{data.title}</p> */}
 
-            {/* {
-          <SearchDropDown
-            setSearchValue={setSearchValue}
-            setIsOpen={setIsOpen}
-            isOpen={isOpen}
-            filteredArticle={filteredArticle}
-          />
-        } */}
+            {
+              <SearchDropDown
+                setSearchValue={setSearchValue}
+                setIsOpen={setIsOpen}
+                isOpen={isOpen}
+                filteredProperty={filteredProperty}
+              />
+            }
           </div>
         </div>
         <Map />
@@ -45,3 +78,76 @@ export default function Home() {
 // https://maps.googleapis.com/maps/api/js?key=AIzaSyBeK6kNAnqtWp2xZpPHmcXOjESpg5cu6ek&libraries=places&language=en
 
 // AIzaSyBeK6kNAnqtWp2xZpPHmcXOjESpg5cu6ek
+
+// const [articlesForSearch, setArticlesForSearch] = useState([]);
+// const [searchValue, setSearchValue] = useState("");
+// const [isOpen, setIsOpen] = useState(false);
+
+// if (typeof window !== "undefined") {
+//   document.addEventListener("mouseup", () => {
+//     handleCloseDropDown();
+//   });
+// }
+// const filteredArticle = articlesForSearch.filter((article) =>
+//   article?.title?.toLowerCase().includes(searchValue)
+// );
+
+// const handleCloseDropDown = () => {
+//   setIsOpen(false);
+// };
+
+// const handleInputChange = (event) => {
+//   setIsOpen(true);
+//   setSearchValue(event.target.value);
+// };
+
+// const fetchSearchData = () => {
+//   fetch(`https://dev.to/api/articles?per_page=100`)
+//     .then((response) => response.json())
+//     .then((data) => setArticlesForSearch(data));
+// };
+
+// useEffect(() => {
+//   fetchSearchData();
+// }, []);
+
+// return (
+//   <main className="pb-8 pt-8">
+//     <div className="w-full flex justify-around relative">
+//       <div className="container flex justify-between items-center px-8">
+//         <Link href="/">
+//           <LogoIcon />
+//         </Link>
+//         <div className="flex items-center justify-center ">
+//           <Link href="/">
+//             <button className="px-2">Home</button>
+//           </Link>
+//           <Link href="/blogs">
+//             <button className="px-2">Blog</button>
+//           </Link>
+//           <Link href="/contact-us">
+//             <button className="px-2">Contact</button>
+//           </Link>
+//         </div>
+//         <div className="flex rounded-md bg-[#E8E8EA] text-wrap gap-2 pr-2 pl-3 pb-2 pt-2 items-center relative">
+//           <input
+//             placeholder="Search"
+//             value={searchValue}
+//             type="text"
+//             className="bg-[#E8E8EA] outline-none rounded-md"
+//             onChange={handleInputChange}
+//           />
+//           {
+//             <SearchDropDown
+//               setSearchValue={setSearchValue}
+//               setIsOpen={setIsOpen}
+//               isOpen={isOpen}
+//               filteredArticle={filteredArticle}
+//             />
+//           }
+//           <SearchIcon />
+//         </div>
+//       </div>
+//     </div>
+//   </main>
+// );
--- src/components/search/SearchDropDown.jsx
+++ src/components/search/SearchDropDown.jsx
@@ -1,29 +1,29 @@
 import Link from "next/link";
 
 export const SearchDropDown = ({
-  filteredArticle,
+  filteredProperty,
   isOpen,
   setIsOpen,
   setSearchValue,
 }) => {
-  filteredArticle.length = 5;
+  filteredProperty.length = 5;
 
-  const handleClickLink = () => {
-    setIsOpen(false);
-    setSearchValue("");
-  };
+  // const handleClickLink = () => {
+  //   setIsOpen(false);
+  //   setSearchValue("");
+  // };
 
   return (
     <div
       className={`${
-        isOpen ? "h-200 border" : "h-0"
+        isOpen ? "h-500 border" : "h-0"
       } flex flex-col gap-1 transition-all duration-200 overflow-hidden absolute rounded-2xl bg-white`}
     >
-      {filteredArticle.map((article) => {
+      {filteredProperty.map((properties) => {
         return (
-          <Link onClick={setIsOpen} href={`/blogs/${article?.id}`}>
+          <Link onClick={setIsOpen} href={`/mock/${properties?.id}`}>
             <div className="p-3 border  rounded-xl text-wrap hover:bg-blue-100">
-              {article?.title}
+              {properties?.title}
             </div>
           </Link>
         );
